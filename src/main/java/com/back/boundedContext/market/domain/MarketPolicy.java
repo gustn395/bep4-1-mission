70543278ebfffa336bc495069935e503968bb818
gustn395 <gustn395@naver.com>
2026-09-22 11:42:53 +0900

    0037 각종 설정들을 application.yml 파일에 모아두기

--- src/main/java/com/back/boundedContext/market/domain/MarketPolicy.java
+++ src/main/java/com/back/boundedContext/market/domain/MarketPolicy.java
@@ -1,5 +1,17 @@
 package com.back.boundedContext.market.domain;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Service
 public class MarketPolicy {
-    public static final double PRODUCT_PAYOUT_RATE = 90;
+    public static double PRODUCT_PAYOUT_RATE;
+
+    @Value("${custom.market.product.payoutRate}")
+    public void setProductPayoutRate(double rate) {
+        PRODUCT_PAYOUT_RATE = rate;
+    }
 }
